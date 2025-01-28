a85191eed31f3ee4cb1924892208f7c49ca505e7
Aperson <69006803+aperson5647@users.noreply.github.com>
2025-01-28 16:11:06 +0000

    Name changes

--- SKIDSCII_Randomize.lua
+++ SKIDSCII_Randomize.lua
@@ -1,1 +1,14 @@
-local template = io.open("SKIDSCII_Dictionary.skid", "r")
+-- DOES NOTHING RIGHT NOW!!!
+
+io = require("io")
+
+local template = io.open("SKIDSCII_Template.txt", "r")
+
+if template then
+    local tempDATA = template:read("a")
+    template:close()
+
+    print(tempDATA)
+end
+
+-- randomly assign each line to random spot in dictionary
